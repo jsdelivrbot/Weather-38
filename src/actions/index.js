6bf4269d9f67f909de380c2914d8e0d042944cf0
AtomCB <acbritto@gmail.com>
2018-07-18 13:22:47 -0700

    Add weather list container.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = "e915ebbe154358d164a176f058f65340";
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
